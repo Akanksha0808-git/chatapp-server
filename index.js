cbcdf7926478a6010b51ea6b6580003c0f697b70
Akanksha <akanksharma0829@gmail.com>
2023-09-22 02:27:55 +0530

    finally commit

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 const express = require('express');
 // const http = require('http');
 const socketIo = require('socket.io');
-
+port = 5000
 const app = express();
-// const server = http.createServer(app);
-
-const PORT = 5000;
-
-const server=app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-const io = socketIo(server,{
-  cors:{
-    origin:"*",
-  }
+const server = app.listen(port, () => {
+    console.log(`server running fine in port no.${port}`)
+})
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+    }
 });
 
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+   
 
-io.on('connection', (client) => {
-  console.log(`${client.id} is connected now`);
-
- 
-    client.on('GROUP', (messageData) => {
-      console.log(messageData.Message, messageData.userName);
-      if (messageData.userName !== '') {
-        client.to(data.GroupName).emit('SENDINGROUP', messageData);
-      }
+    socket.on('chatMessage', (message) => {
+        io.emit('chatMessage', message); // Broadcast the message to all connected clients
     });
-    
-
-});
+  });
+  app.get("/", (req,res)=>{
+    res.send("")
+}) ;
 
