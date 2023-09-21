8934062ef6a67bbce605901a02bc15e118271e4e
Akanksha <akanksharma0829@gmail.com>
2023-09-22 00:47:23 +0530

    final commit

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const express = require('express');
-const http = require('http');
+// const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+// const server = http.createServer(app);
 
 const PORT = 5000;
 
-server.listen(PORT, () => {
+const server=app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+const io = socketIo(server,{
+  cors:{
+    origin:"*",
+  }
+});
+
 
 io.on('connection', (client) => {
   console.log(`${client.id} is connected now`);
 
-  client.on('JOIN_GROUP', (data) => {
-    client.join(data.GroupName);
-    console.log(data.GroupName, data.userName);
-    io.to(data.GroupName).emit('JOIN_MESSAGE', `${data.userName} joined the Group`);
-
-    client.on('SENDINGROUP', (messageData) => {
+ 
+    client.on('GROUP', (messageData) => {
       console.log(messageData.Message, messageData.userName);
       if (messageData.userName !== '') {
         client.to(data.GroupName).emit('SENDINGROUP', messageData);
       }
     });
-  });
+    
+
 });
 
